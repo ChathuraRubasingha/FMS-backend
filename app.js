c0f71623c8b2b8173e65f1642dae6e5343d52237
ChathuraRubasingha <rubasingharcpk.19@uom.lk>
2022-06-06 12:57:30 +0530

    updated registory module

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const res = require('express/lib/response');
 const { DEC8_BIN } = require('mysql/lib/protocol/constants/charsets');
 const routes = require("./routes/Driver")
 const routesMaintanance =require("./routes/Maintanace")
+const routesRegisted = require('./routes/Registory')
+
 const app = express()
 app.use(cors());
 app.use(express.json());
@@ -16,5 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use("/api",routes)
 app.use("/api",routesMaintanance)
+app.use('/api',routesRegisted)
 //Listern on enviroment port or 5000
 app.listen(port, () => console.log(`Listern on port ${port}`))
