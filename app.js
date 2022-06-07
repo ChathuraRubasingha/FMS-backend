4e69e702b4576a7499aa6f25dab37c853557a93b
DilkaSithari <dilkasithari255@gmail.com>
2022-06-07 07:52:46 +0530

    Updated Fuel consumption, maintainance cost, driver performance repair, vehicle detail in reports module and branch registry in configuration module

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const Connection = require('mysql/lib/Connection')
+const express = require("express");
+const bodyParser = require("body-parser");
+const Connection = require("mysql/lib/Connection");
 const cors = require("cors");
-const res = require('express/lib/response');
-const { DEC8_BIN } = require('mysql/lib/protocol/constants/charsets');
-const routes = require("./routes/Driver")
-const routesMaintanance =require("./routes/Maintanace")
-const app = express()
+const res = require("express/lib/response");
+const { DEC8_BIN } = require("mysql/lib/protocol/constants/charsets");
+const routes = require("./routes/Driver");
+const routesMaintanance = require("./routes/Maintanace");
+const routesRepair = require("./routes/Repair");
+const routesMaintainanceCost = require("./routes/MaintainanceCost");
+const routesFuelConsumption = require("./routes/FuelConsumption");
+const routesPerformanceDriver = require("./routes/PerformanceDriver");
+const routesVehicleDetail = require("./routes/VehicleDetail");
+const routesBranch = require("./routes/Branch");
+const app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
-app.use("/api",routes)
-app.use("/api",routesMaintanance)
+app.use(bodyParser.json());
+app.use("/api", routes);
+app.use("/api", routesMaintanance);
+app.use("/api", routesRepair);
+app.use("/api", routesMaintainanceCost);
+app.use("/api", routesFuelConsumption);
+app.use("/api", routesPerformanceDriver);
+app.use("/api", routesVehicleDetail);
+app.use("/api", routesBranch);
 //Listern on enviroment port or 5000
-app.listen(port, () => console.log(`Listern on port ${port}`))
+app.listen(port, () => console.log(`Listern on port ${port}`));
---
+++ controllers/Branch_controller.js
@@ -0,0 +1,52 @@
+const pool = require("../db");
+const express = require("express"),
+	router = express.Router();
+
+const AddBranch = (req, res) => {
+	console.log(req.body);
+	const location = req.body.location;
+	const branch = req.body.branch;
+
+	pool.query(
+		"INSERT INTO ma_location (Location_Name)VALUES(?),INSERT INTO ma_branch (Branch)VALUES(?)",
+		[location, branch],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send("Success");
+			}
+		}
+	);
+};
+
+// get data from database
+const GetBranch = (req, res) => {
+	pool.query(
+		"SELECT    ma_branch.Branch,    ma_location.Location_Name   FROM  ma_location    INNER JOIN ma_branch ON ma_branch.Location_ID = ma_location.Location_ID",
+		(err, rows) => {
+			if (!err) {
+				res.send(rows);
+			} else {
+				console.log(err);
+			}
+		}
+	);
+};
+
+//delete data
+// const DeleteById= (req, res) =>{
+//     const id = req.params.id
+//     console.log(id)
+//     pool.query('DELETE FROM ma_driver WHERE Driver_ID = ?', id, (err, result) => {
+//         if(err) {
+//             console.log(err)
+//         }else{
+//             res.send(result);
+//         }
+//     })
+// }
+
+exports.AddBranch = AddBranch;
+// exports.DeleteById=DeleteById
+exports.GetBranch = GetBranch;
